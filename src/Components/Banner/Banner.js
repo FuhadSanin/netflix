a6500a6c0857084dc8dd97edc354f338f3f71151
FuhadSanin <97791494+FuhadSanin@users.noreply.github.com>
2023-02-06 23:59:20 -0800

    Update Banner.js

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
     useEffect(() => {
         axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
 
-            setMovie(response.data.results[0])
+            setMovie(response.data.results[1])
         })
     }, []);
     return (
